567839aa920c2f9d2e25cf090d9c47f566c8e61b
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-03-25 18:49:20 +0700

    show data from file.txt

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link href="{{asset('bootstrap-5/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="{{asset('bootstrap-5/js/bootstrap.bundle.min.js')}}" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <div class="container">
+        <h1>Blog Riski</h1>
+
+        @foreach($posts as $post)
+            @php($post = explode(",",$post))
+            <div class="card mb-3"> 
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post[1] }}</h5>
+                    <p class="card-text">{{$post[2]}}</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated {{ date('d M Y H:i', strtotime($post[3]))}}</small></p>
+                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('hello', 'App\Http\Controllers\HelloController@index');
+// Route::get('hello', [HelloController::class, 'index']);
+Route::get('posts', [PostController::class, 'index']);
